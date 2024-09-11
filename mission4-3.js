25f07063ea660ba15918bdd1e6f8c781486b5359
s2410131 <s2410131@jaist.ac.jp>
2024-09-11 19:56:42 +0900

    Add files via upload

---
+++ mission4-3.js
@@ -0,0 +1,10 @@
+var omikuji = ["大吉","中吉","小吉"]
+		
+var number1 = Math.random();
+var number2 = number1 * 3;
+var number = Math.floor(number2);
+var message = omikuji[number];
+		
+var object = document.getElementById("omikuji");
+object.innerText = message;
+alert("おみくじを引きます");
